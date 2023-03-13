10353cbd9cfafcfa3b9d8126b394e668789f90ff
JosephKingsley <chikingcj@gmail.com>
2023-03-13 09:42:49 +0000

    signin page updated

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -15,22 +15,22 @@ export default function SigninPage() {
 
   const onsubmit = async (event) => {
     setErrors('')
+    console.log()
     event.preventDefault();
-    try {
       Auth.signIn(email, password)
         .then(user => {
           localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
           window.location.href = "/"
         })
-        .catch(err => { console.log('Error!', err) });
-    } catch (error) {
+    .catch(error => {
       if (error.code == 'UserNotConfirmedException') {
         window.location.href = "/confirm"
       }
-      setCognitoErrors(error.message)
-    }
+      setErrors(error.message)
+    });
     return false
   }
+  
   
   const email_onchange = (event) => {
     setEmail(event.target.value);
